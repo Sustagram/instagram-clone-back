53e836a04e0c2cec9984e55f920d142b68930283
SkyLightQP <combbm@naver.com>
2020-09-11 18:25:24 +0900

    fix: apply env at DATABASES

--- instagramBackend/settings.py
+++ instagramBackend/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'instagramBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_DATABASE"),
+        'USER': os.environ.get("DB_USERNAME"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT")
     }
 }
 
