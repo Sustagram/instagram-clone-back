26214196c64260a79fe035cc4c7550c55e982c24
SkyLightQP <combbm@naver.com>
2020-09-11 18:55:32 +0900

    fix: add upper() function in DEBUG option

--- instagramBackend/settings.py
+++ instagramBackend/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG") == 'TRUE'
+DEBUG = os.environ.get("DEBUG").upper() == 'TRUE'
 ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
