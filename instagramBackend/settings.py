971fea1f3fddd5c00d78b059b5ae36cd72166dd0
combbm <combbm@naver.com>
2020-09-18 17:59:05 +0900

    feat: 앱 추가

--- instagramBackend/settings.py
+++ instagramBackend/settings.py
@@ -18,6 +18,8 @@ CORS_ALLOW_METHODS = (
 )
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
