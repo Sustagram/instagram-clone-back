f4a5b0e097b4b881b1796b3d12b9e63198168db1
combbm <combbm@naver.com>
2020-09-20 23:47:51 +0900

    feat: implement login api without JWT

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Register
+from .views import Register, Login
 
 urlpatterns = [
-    path('register/', Register.as_view())
+    path('register/', Register.as_view()),
+    path('login/', Login.as_view())
 ]
