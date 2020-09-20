86eca6882a1e5c01afae1d4403af4f655f70d55e
combbm <combbm@naver.com>
2020-09-20 23:42:26 +0900

    feat: implement register api

--- api/views.py
+++ api/views.py
@@ -1,25 +1,28 @@
-import datetime
 import json
 
-import uuid
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .errors import USER_ALREADY_EXISTS
 from .models import User
+from .serializers import UserSerializer
+from .utils import make_response_payload
 
 
 class Register(APIView):
-    def post(self, request, format=None):
+    def post(self, request):
         if request.META["CONTENT_TYPE"] != "application/json":
             return
+
         body = json.loads(request.body)
-        user = User(
-            user_id=uuid.uuid4(),
-            email=body["email"],
-            realname=body["realname"],
-            username=body["username"],
-            password=body["password"],
-            last_login=datetime.datetime.now()
-        )
-        user.save()
-        return Response(status=201)
+
+        if User.objects.filter(email=body["email"]).exists():
+            return Response(make_response_payload(is_success=False, message=USER_ALREADY_EXISTS), status=409)
+
+        user = User.objects.create_user(email=body["email"], realname=body["realname"],
+                                        username=body["username"],
+                                        password=body["password"])
+
+        result = UserSerializer(user).data
+
+        return Response(make_response_payload(result))
