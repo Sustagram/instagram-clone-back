c02caee5ec01852de9233bc7027975cc5c72b381
combbm <combbm@naver.com>
2020-10-15 20:48:25 +0900

    feat: add serializer for post model

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ['password']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
