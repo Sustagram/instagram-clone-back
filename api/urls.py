be7e38dc9bf948582820ad09c1f99a72470667cf
combbm <combbm@naver.com>
2020-10-22 21:40:07 +0900

    feat: implement /follow api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Register, Login, Me, PostAPI
+from .views import Register, Login, Me, PostAPI, Follow
 
 urlpatterns = [
     path('register/', Register.as_view()),
     path('login/', Login.as_view()),
     path('me/', Me.as_view()),
-    path('post/', PostAPI.as_view())
+    path('post/', PostAPI.as_view()),
+    path('follow/', Follow.as_view())
 ]
