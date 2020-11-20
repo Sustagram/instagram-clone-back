7c99b091af7b7d6383a88b93de60050f81c06222
combbm <combbm@naver.com>
2020-11-20 16:49:00 +0900

    fix: rename getting post api to /post/follow/

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('register/', auth.Register.as_view()),
     path('login/', auth.Login.as_view()),
     path('me/', auth.Me.as_view()),
-    path('post/', post.PostAPI.as_view()),
+
+    path('post/follow/', post.PostAPI.as_view()),
     path('post/my/', post.MyPostAPI.as_view()),
+    
     path('follow/', follow.Follow.as_view())
 ]
