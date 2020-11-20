b00950201dd7f33fcd618dcf34b93ff11ed83fbe
combbm <combbm@naver.com>
2020-11-20 17:30:30 +0900

    feat: implement GET /follow/ to get my follow list

--- api/routes/follow.py
+++ api/routes/follow.py
@@ -12,6 +12,20 @@ from ..utils import make_response_payload, require_token
 
 class Follow(APIView):
     @require_token
+    def get(self, request):
+        if request.META["CONTENT_TYPE"] != "application/json":
+            return Response(make_response_payload(is_success=False), status=415)
+
+        user_id = request.user['user_id']
+        my_follow = Subscribe.objects.filter(user_id=user_id).all()
+
+        result = []
+        for value in my_follow:
+            result.append(SubscribeSerializer(value).data)
+
+        return Response(make_response_payload(result), status=200)
+
+    @require_token
     def post(self, request):
         if request.META["CONTENT_TYPE"] != "application/json":
             return Response(make_response_payload(is_success=False), status=415)
